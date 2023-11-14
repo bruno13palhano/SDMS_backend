7947d1ed69a4b943aa8cc788a6f89a7e8b6dacae
bruno13palhano <bruno13palhano@gmail.com>
2023-11-13 21:19:43 -0300

    Add a model for Stock and Orders items

--- model/src/main/java/com/bruno13palhano/model/StockOrder.java
+++ model/src/main/java/com/bruno13palhano/model/StockOrder.java
@@ -1,4 +1,142 @@
 package com.bruno13palhano.model;
 
+import java.util.List;
+
 public class StockOrder {
+    private Long id;
+    private Long productId;
+    private String name;
+    private byte[] photo;
+    private Long date;
+    private Long validity;
+    private Integer quantity;
+    private List<Category> categories;
+    private String company;
+    private Float purchasePrice;
+    private Float salePrice;
+    private Boolean isOrderedByCustomer;
+    private Boolean isPaid;
+
+    public StockOrder() {}
+
+    public StockOrder(Long id, Long productId, String name, byte[] photo, Long date, Long validity, Integer quantity,
+                      List<Category> categories, String company, Float purchasePrice, Float salePrice,
+                      Boolean isOrderedByCustomer, Boolean isPaid) {
+        this.id = id;
+        this.productId = productId;
+        this.name = name;
+        this.photo = photo;
+        this.date = date;
+        this.validity = validity;
+        this.quantity = quantity;
+        this.categories = categories;
+        this.purchasePrice = purchasePrice;
+        this.salePrice = salePrice;
+        this.isOrderedByCustomer = isOrderedByCustomer;
+        this.isPaid = isPaid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public Long getValidity() {
+        return validity;
+    }
+
+    public void setValidity(Long validity) {
+        this.validity = validity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Float getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(Float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public Boolean getOrderedByCustomer() {
+        return isOrderedByCustomer;
+    }
+
+    public void setOrderedByCustomer(Boolean orderedByCustomer) {
+        isOrderedByCustomer = orderedByCustomer;
+    }
+
+    public Boolean getPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(Boolean paid) {
+        isPaid = paid;
+    }
 }
