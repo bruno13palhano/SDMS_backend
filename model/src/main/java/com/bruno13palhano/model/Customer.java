16dd7d90ffc58a9efcfadc043d9ab94324501074
bruno13palhano <bruno13palhano@gmail.com>
2023-11-13 21:18:35 -0300

    Add a model for Customers

--- model/src/main/java/com/bruno13palhano/model/Customer.java
+++ model/src/main/java/com/bruno13palhano/model/Customer.java
@@ -1,2 +1,69 @@
-package com.bruno13palhano.model;public class Customer {
+package com.bruno13palhano.model;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private byte[] photo;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    public Customer() {}
+
+    public Customer(Long id, String name, byte[] photo, String email, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.photo = photo;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
