12ca90979fc0cf2026dc91ad1607b7db1bc1320a
bruno13palhano <bruno13palhano@gmail.com>
2023-11-15 20:21:45 -0300

    Remove unused methods

--- core/src/main/java/com/bruno13palhano/data/Utils.java
+++ core/src/main/java/com/bruno13palhano/data/Utils.java
@@ -1,40 +1,24 @@
 package com.bruno13palhano.data;
 
 import com.bruno13palhano.data.model.CategoryEntity;
-import com.bruno13palhano.data.model.ProductCategoriesEmbeddable;
 import com.bruno13palhano.data.model.ProductEntity;
 import com.bruno13palhano.model.Category;
 import com.bruno13palhano.model.Product;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Utils {
 
     public static Category categoryAsExternal(CategoryEntity entity) {
         return new Category(
                 entity.getId(),
-                entity.getName()
+                entity.getCategory()
         );
     }
 
     public static CategoryEntity categoryAsEntity(Category external) {
         return new CategoryEntity(
                 external.getId(),
-                external.getName()
+                external.getCategory()
         );
-    }
-
-    public static List<Category> categoriesAsExternal(ProductCategoriesEmbeddable entity) {
-        List<Category> result = new ArrayList<>();
-        entity.getCategories().forEach( c -> result.add(new Category(c.getId(), c.getName())));
-        return result;
-    }
-
-    public static ProductCategoriesEmbeddable categoriesAsEntity(List<Category> external, Long productId) {
-        List<CategoryEntity> categories = new ArrayList<>();
-        external.forEach( e -> categories.add(new CategoryEntity(e.getId(), e.getName())));
-        return new ProductCategoriesEmbeddable(categories);
     }
 
     public static Product productAsExternal(ProductEntity entity) {
@@ -45,7 +29,7 @@ public class Utils {
             entity.getDescription(),
             entity.getPhoto(),
             entity.getDate(),
-            categoriesAsExternal(entity.getCategories()),
+            entity.getCategories(),
             entity.getCompany()
         );
     }
@@ -58,7 +42,7 @@ public class Utils {
                 external.getDescription(),
                 external.getPhoto(),
                 external.getDate(),
-                categoriesAsEntity(external.getCategories(), external.getId()),
+                external.getCategories(),
                 external.getCompany()
         );
     }
