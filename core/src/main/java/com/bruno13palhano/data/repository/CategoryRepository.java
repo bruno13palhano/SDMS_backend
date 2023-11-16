c1e5fc8670ea16c1e1ea6a02013a6ad92796dd7c
bruno13palhano <bruno13palhano@gmail.com>
2023-11-16 11:39:09 -0300

    Remove CategoryDao

--- core/src/main/java/com/bruno13palhano/data/repository/CategoryRepository.java
+++ core/src/main/java/com/bruno13palhano/data/repository/CategoryRepository.java
@@ -1,40 +1,93 @@
 package com.bruno13palhano.data.repository;
 
+import com.bruno13palhano.data.ConnectionFactory;
 import com.bruno13palhano.data.Repository;
-import com.bruno13palhano.data.Utils;
-import com.bruno13palhano.data.dao.CategoryDAO;
 import com.bruno13palhano.model.Category;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.stream.StreamSupport;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class CategoryRepository implements Repository<Category> {
-    private final CategoryDAO categoryDAO;
-
-    public CategoryRepository(CategoryDAO categoryDAO) {
-        this.categoryDAO = categoryDAO;
-    }
-
     @Override
     public void insert(Category data) {
-        categoryDAO.save(Utils.categoryAsEntity(data));
+        String QUERY = "INSERT INTO category_table (id, category) VALUES (?,?)";
+
+        Connection connection = new ConnectionFactory().getConnection();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+            preparedStatement.setLong(1, data.getId());
+            preparedStatement.setString(2, data.getCategory());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void update(Category data) {
-        categoryDAO.update(Utils.categoryAsEntity(data));
+        String QUERY = "UPDATE category_table SET category = ? WHERE id = ?";
+
+        Connection connection = new ConnectionFactory().getConnection();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+            preparedStatement.setString(1, data.getCategory());
+            preparedStatement.setLong(2, data.getId());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteById(Long id) {
-        categoryDAO.deleteById(id);
+        String QUERY = "DELETE FROM category_table WHERE id = ?";
+
+        Connection connection = new ConnectionFactory().getConnection();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Iterable<Category> getAll() {
-        return StreamSupport
-                .stream(categoryDAO.findAll().spliterator(), false)
-                .map(Utils::categoryAsExternal).toList();
+        List<Category> result = new ArrayList<>();
+        String QUERY = "SELECT * FROM category_table";
+
+        Connection connection = new ConnectionFactory().getConnection();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                result.add(
+                        new Category(
+                                resultSet.getLong("id"),
+                                resultSet.getString("category")
+                        )
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
