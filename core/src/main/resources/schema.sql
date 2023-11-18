dbaedbe45ebc498dcbbea803cbb00dec6a8fb5b4
bruno13palhano <bruno13palhano@gmail.com>
2023-11-17 21:22:03 -0300

    Add sale_table and delivery_tabel to schema

--- core/src/main/resources/schema.sql
+++ core/src/main/resources/schema.sql
@@ -34,10 +34,10 @@ CREATE TABLE IF NOT EXISTS stock_order_table (
     is_ordered_by_customer boolean,
     is_paid boolean,
     PRIMARY KEY (id),
-    FOREIGN KEY (product_id) REFERENCES product_table (id) ON DELETE CASCADE
-)
+    FOREIGN KEY (product_id) REFERENCES product_table (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE TABLE customer_table (
+CREATE TABLE IF NOT EXISTS customer_table (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255),
     photo varbinary(255),
@@ -45,4 +45,31 @@ CREATE TABLE customer_table (
     address varchar(255),
     phone_number varchar(255),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS sale_table (
+    id int NOT NULL AUTO_INCREMENT,
+    product_id int NOT NULL,
+    stock_order_id int NOT NULL,
+    customer_id int NOT NULL,
+    quantity int,
+    purchase_price float,
+    sale_price float,
+    date_of_sale bigint,
+    date_of_payment bigint,
+    is_ordered_by_customer boolean,
+    is_paid_by_customer boolean,
+    canceled boolean,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS delivery_table (
+    id int NOT NULL AUTO_INCREMENT,
+    sale_id int NOT NULL,
+    delivery_price float,
+    shipping_date bigint,
+    delivery_date bigint,
+    delivered boolean,
+    PRIMARY KEY (id),
+    FOREIGN KEY (sale_id) REFERENCES sale_table (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
