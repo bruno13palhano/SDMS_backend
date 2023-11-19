9012118816af20a3f62f6b666d37e84d592b1bf1
bruno13palhano <bruno13palhano@gmail.com>
2023-11-18 22:26:00 -0300

    Refactor insert method and add paths for update and delete Category

--- src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CategoryController.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CategoryController.java
@@ -3,10 +3,7 @@ package com.bruno13palhano.shopdani_stock_management.controllers;
 import com.bruno13palhano.data.repository.CategoryRepository;
 import com.bruno13palhano.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/categories")
 @RestController
@@ -20,8 +17,18 @@ public class CategoryController {
         return categoryRepository.getAll();
     }
 
-    @GetMapping("/insert")
-    void insert() {
-        categoryRepository.insert(new Category(0L, "Masculino"));
+    @PostMapping(path = "/insert")
+    void insert(@RequestBody Category category) {
+        categoryRepository.insert(category);
+    }
+
+    @PutMapping(path = "/update")
+    void update(@RequestBody Category category) {
+        categoryRepository.update(category);
+    }
+
+    @GetMapping("/delete/{id}")
+    void delete(@PathVariable Long id) {
+        categoryRepository.deleteById(id);
     }
 }
