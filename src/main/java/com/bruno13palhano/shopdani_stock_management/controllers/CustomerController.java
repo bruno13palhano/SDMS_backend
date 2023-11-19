b3a161df2afc3aa97122a243656ad2d5aa44a84e
bruno13palhano <bruno13palhano@gmail.com>
2023-11-18 22:43:56 -0300

    Refactor insert method and add paths for update and delete Customer

--- src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CustomerController.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CustomerController.java
@@ -3,10 +3,7 @@ package com.bruno13palhano.shopdani_stock_management.controllers;
 import com.bruno13palhano.data.repository.CustomerRepository;
 import com.bruno13palhano.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/customers")
 @RestController
@@ -16,18 +13,19 @@ public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
-    @GetMapping("/insert")
-    void insert() {
-        customerRepository.insert(
-                new Customer(
-                        0L,
-                        "Bruno Barbosa",
-                        new byte[] {},
-                        "bruno13palhano@gmail.com",
-                        "Rua 15 de Novembro",
-                        "1111111"
-                )
-        );
+    @PostMapping(path = "/insert")
+    void insert(@RequestBody Customer customer) {
+        customerRepository.insert(customer);
+    }
+
+    @PutMapping(path = "/update")
+    void update(@RequestBody Customer customer) {
+        customerRepository.update(customer);
+    }
+
+    @GetMapping("/delete/{id}")
+    void delete(@PathVariable Long id) {
+        customerRepository.deleteById(id);
     }
 
     @GetMapping("/all")
