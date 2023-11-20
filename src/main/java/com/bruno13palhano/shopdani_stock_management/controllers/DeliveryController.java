d948b6898db651b71df9099788cb027145d50f45
bruno13palhano <bruno13palhano@gmail.com>
2023-11-20 20:01:40 -0300

    Add insert, update and delete methods

--- src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/DeliveryController.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/DeliveryController.java
@@ -3,10 +3,7 @@ package com.bruno13palhano.shopdani_stock_management.controllers;
 import com.bruno13palhano.data.repository.DeliveryRepository;
 import com.bruno13palhano.model.Delivery;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/deliveries")
 @RestController
@@ -16,22 +13,23 @@ public class DeliveryController {
     @Autowired
     private DeliveryRepository deliveryRepository;
 
-    @GetMapping("/insert")
-    void insert() {
-        deliveryRepository.insert(
-                new Delivery(
-                        0L,
-                        1L,
-                        "",
-                        "",
-                        "",
-                        "",
-                        0F,
-                        1.2F,
-                        1122L,
-                        2211L,
-                        false
-                )
-        );
+    @PostMapping(path = "/insert")
+    void insert(@RequestBody Delivery delivery) {
+        deliveryRepository.insert(delivery);
+    }
+
+    @PutMapping(path = "/update")
+    void update(@RequestBody Delivery delivery) {
+        deliveryRepository.update(delivery);
+    }
+
+    @GetMapping(path = "/delete/{id}")
+    void delete(@PathVariable Long id) {
+        deliveryRepository.deleteById(id);
+    }
+
+    @GetMapping(path = "/all")
+    Iterable<Delivery> getAll() {
+        return deliveryRepository.getAll();
     }
 }
