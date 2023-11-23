9c8c67d74ab1f5f38eb52e9c8a54d79d299cc30c
bruno13palhano <bruno13palhano@gmail.com>
2023-11-23 19:15:42 -0300

    PasswordEnconder test

--- src/main/java/com/bruno13palhano/shopdani_stock_management/ShopdaniStockManagementApplication.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/ShopdaniStockManagementApplication.java
@@ -3,6 +3,7 @@ package com.bruno13palhano.shopdani_stock_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bruno13palhano"})
@@ -10,6 +11,12 @@ public class ShopdaniStockManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopdaniStockManagementApplication.class, args);
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "";
+		String encodedPassword = encoder.encode(rawPassword);
+
+		System.out.println(encodedPassword);
 	}
 
 }
