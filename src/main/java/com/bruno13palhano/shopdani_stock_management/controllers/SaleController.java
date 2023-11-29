fdea53166e0f58e89488b13c8f40d1b48021fdb5
bruno13palhano <bruno13palhano@gmail.com>
2023-11-28 22:35:30 -0300

    Change param from insert method to SaleItems

--- src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/SaleController.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/SaleController.java
@@ -2,9 +2,13 @@ package com.bruno13palhano.shopdani_stock_management.controllers;
 
 import com.bruno13palhano.data.repository.SaleRepository;
 import com.bruno13palhano.data.repository.StockOrderRepository;
+import com.bruno13palhano.model.Delivery;
 import com.bruno13palhano.model.Sale;
+import com.bruno13palhano.model.StockOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RequestMapping("/sales")
 @RestController
@@ -18,8 +22,12 @@ public class SaleController {
     private SaleRepository saleRepository;
 
     @PostMapping(path = "/insert")
-    void insert(@RequestBody Sale sale) {
-        saleRepository.insert(sale);
+    void insert(@RequestBody SaleItems saleItems) {
+        Sale sale = saleItems.sale;
+        StockOrder stockOrder = saleItems.stockOrder;
+        Delivery delivery = saleItems.delivery;
+
+        saleRepository.insertItems(sale, stockOrder, delivery);
     }
 
     @PutMapping(path = "/update")
@@ -35,5 +43,43 @@ public class SaleController {
     @GetMapping("/all")
     Iterable<Sale> getAll() {
         return saleRepository.getAll();
+    }
+
+    static class SaleItems {
+        private Sale sale;
+        private StockOrder stockOrder;
+        private Delivery delivery;
+
+        SaleItems() {}
+
+        SaleItems(Sale sale, StockOrder stockOrder, Delivery delivery) {
+            this.sale = sale;
+            this.stockOrder = stockOrder;
+            this.delivery = delivery;
+        }
+
+        public Sale getSale() {
+            return sale;
+        }
+
+        public void setSale(Sale sale) {
+            this.sale = sale;
+        }
+
+        public StockOrder getStockOrder() {
+            return stockOrder;
+        }
+
+        public void setStockOrder(StockOrder stockOrder) {
+            this.stockOrder = stockOrder;
+        }
+
+        public Delivery getDelivery() {
+            return delivery;
+        }
+
+        public void setDelivery(Delivery delivery) {
+            this.delivery = delivery;
+        }
     }
 }
