608ce4ce9be722abacd72a7db8aace0347d1ea6a
bruno13palhano <bruno13palhano@gmail.com>
2023-11-30 19:16:42 -0300

    Add a Service for Customer

---
+++ core/src/main/java/com/bruno13palhano/data/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.bruno13palhano.data.service;
+
+import com.bruno13palhano.data.Repository;
+import com.bruno13palhano.data.repository.CustomerRepository;
+import com.bruno13palhano.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService implements Repository<Customer> {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void insert(Customer data) {
+        customerRepository.insert(data);
+    }
+
+    @Override
+    public void update(Customer data) {
+        customerRepository.update(data);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return customerRepository.getAll();
+    }
+}
