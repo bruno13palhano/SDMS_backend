26ebd4bfc35e5c61db1a8c9985875459dd9f75c4
bruno13palhano <bruno13palhano@gmail.com>
2023-11-30 19:33:57 -0300

    Add a Service for Product

---
+++ core/src/main/java/com/bruno13palhano/data/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.bruno13palhano.data.service;
+
+import com.bruno13palhano.data.Repository;
+import com.bruno13palhano.data.repository.ProductRepository;
+import com.bruno13palhano.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService implements Repository<Product> {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public void insert(Product data) {
+        productRepository.insert(data);
+    }
+
+    @Override
+    public void update(Product data) {
+        productRepository.update(data);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+}
