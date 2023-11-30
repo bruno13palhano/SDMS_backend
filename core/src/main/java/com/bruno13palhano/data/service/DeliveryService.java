4ae70df83b50340761c2468c7e20d1d116a12e37
bruno13palhano <bruno13palhano@gmail.com>
2023-11-30 19:25:03 -0300

    Add missing annotation

--- core/src/main/java/com/bruno13palhano/data/service/DeliveryService.java
+++ core/src/main/java/com/bruno13palhano/data/service/DeliveryService.java
@@ -4,9 +4,11 @@ import com.bruno13palhano.data.Repository;
 import com.bruno13palhano.data.repository.DeliveryRepository;
 import com.bruno13palhano.model.Delivery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DeliveryService implements Repository<Delivery> {
     @Autowired
     private DeliveryRepository deliveryRepository;
