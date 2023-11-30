188d24ce2b1718f0c10ed9981eb675b82f0f7b7f
bruno13palhano <bruno13palhano@gmail.com>
2023-11-30 19:01:05 -0300

    Add a Service for Category

---
+++ core/src/main/java/com/bruno13palhano/data/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.bruno13palhano.data.service;
+
+import com.bruno13palhano.data.Repository;
+import com.bruno13palhano.data.repository.CategoryRepository;
+import com.bruno13palhano.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService implements Repository<Category> {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public void insert(Category data) {
+        categoryRepository.insert(data);
+    }
+
+    @Override
+    public void update(Category data) {
+        categoryRepository.update(data);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return categoryRepository.getAll();
+    }
+}
