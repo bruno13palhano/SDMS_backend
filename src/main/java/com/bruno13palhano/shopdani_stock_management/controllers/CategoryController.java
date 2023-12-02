34a27ca0bffc6decd002865a0dbcbbfaab61cc89
bruno13palhano <bruno13palhano@gmail.com>
2023-12-02 18:45:49 -0300

    Refactor CategoryController

--- src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CategoryController.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/controllers/CategoryController.java
@@ -1,37 +1,43 @@
 package com.bruno13palhano.shopdani_stock_management.controllers;
 
-import com.bruno13palhano.data.repository.CategoryRepository;
+import com.bruno13palhano.data.service.impl.DefaultCategoryService;
 import com.bruno13palhano.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping("/categories")
 @RestController
 @CrossOrigin
 public class CategoryController {
     @Autowired
-    private CategoryRepository categoryRepository;
+    private DefaultCategoryService defaultCategoryService;
 
     @GetMapping("/all")
-    ResponseEntity<Iterable<Category>> getAll() {
-        return ResponseEntity.ok().body(categoryRepository.getAll());
+    ResponseEntity<List<Category>> getAll() {
+        return ResponseEntity.ok().body(defaultCategoryService.getAll());
     }
 
     @PostMapping(path = "/insert")
-    void insert(@RequestBody Category category) {
-        categoryRepository.insert(category);
+    public ResponseEntity<?> insert(@RequestBody Category category) {
+        defaultCategoryService.insert(category);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping(path = "/update")
-    void update(@RequestBody Category category) {
-        categoryRepository.update(category);
+    public ResponseEntity<?> update(@RequestBody Category category) {
+        defaultCategoryService.update(category);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Long> delete(@PathVariable Long id) {
-        categoryRepository.deleteById(id);
+        defaultCategoryService.delete(id);
 
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
