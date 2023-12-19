87e77ada0d158c3480fb4bdc39d80fce43071b80
bruno13palhano <bruno13palhano@gmail.com>
2023-12-19 19:54:33 -0300

    Fix missing annotation

--- core/src/main/java/com/bruno13palhano/data/service/impl/DefaultUserService.java
+++ core/src/main/java/com/bruno13palhano/data/service/impl/DefaultUserService.java
@@ -4,7 +4,9 @@ import com.bruno13palhano.data.repository.UserRepository;
 import com.bruno13palhano.data.service.UserService;
 import com.bruno13palhano.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DefaultUserService implements UserService {
     @Autowired
     private UserRepository userRepository;
