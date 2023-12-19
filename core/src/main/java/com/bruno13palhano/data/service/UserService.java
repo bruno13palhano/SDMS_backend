2193fbc05c96736a13f30e95750374d5a920f03d
bruno13palhano <bruno13palhano@gmail.com>
2023-12-19 19:30:36 -0300

    Add a service for user

---
+++ core/src/main/java/com/bruno13palhano/data/service/UserService.java
@@ -0,0 +1,7 @@
+package com.bruno13palhano.data.service;
+
+import com.bruno13palhano.data.UserData;
+import com.bruno13palhano.model.User;
+
+public interface UserService extends UserData<User> {
+}
---
+++ core/src/main/java/com/bruno13palhano/data/service/impl/DefaultUserService.java
@@ -0,0 +1,31 @@
+package com.bruno13palhano.data.service.impl;
+
+import com.bruno13palhano.data.repository.UserRepository;
+import com.bruno13palhano.data.service.UserService;
+import com.bruno13palhano.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DefaultUserService implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void insert(User user) {
+        userRepository.insert(user);
+    }
+
+    @Override
+    public void update(User user) {
+        userRepository.update(user);
+    }
+
+    @Override
+    public void delete(Long userId) {
+        userRepository.delete(userId);
+    }
+
+    @Override
+    public User getById(Long userId) {
+        return userRepository.getById(userId);
+    }
+}
