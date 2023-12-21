e2d1983cdc66b6751a67eb4814747cb1fa362b70
bruno13palhano <bruno13palhano@gmail.com>
2023-12-20 21:30:04 -0300

    Refactor security filter

--- src/main/java/com/bruno13palhano/shopdani_stock_management/SDMSSecurityConfig.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/SDMSSecurityConfig.java
@@ -3,14 +3,19 @@ package com.bruno13palhano.shopdani_stock_management;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +24,18 @@ public class SDMSSecurityConfig {
     @Autowired
     private DataSource dataSource;
 
-    @Autowired void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder())
                 .usersByUsernameQuery("select username, password, enabled from users where username=?")
@@ -28,12 +44,16 @@ public class SDMSSecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-//                .requestMatchers("/**").permitAll()
-                .anyRequest().permitAll())
-//                .authenticated())
-                .csrf(AbstractHttpConfigurer::disable);
-//                .httpBasic();
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/v1/users/insert").permitAll()
+                                .requestMatchers("/v1/users/login").permitAll()
+                                .requestMatchers("/v1/**").authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
 
         return http.build();
     }
