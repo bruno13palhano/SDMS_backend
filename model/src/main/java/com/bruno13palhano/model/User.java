2adb9fcb09c37829c513d1759e1cedf1bccad009
bruno13palhano <bruno13palhano@gmail.com>
2023-12-24 08:18:44 -0300

    User implements UserDetails

--- model/src/main/java/com/bruno13palhano/model/User.java
+++ model/src/main/java/com/bruno13palhano/model/User.java
@@ -1,6 +1,13 @@
 package com.bruno13palhano.model;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
     private Long id;
     private String username;
     private String password;
@@ -38,6 +45,31 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
     }
 
     public String getPassword() {
