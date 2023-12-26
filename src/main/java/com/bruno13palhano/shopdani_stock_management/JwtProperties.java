ca74716271a9e2ac4b9ac5f1de8ce5681331b731
bruno13palhano <bruno13palhano@gmail.com>
2023-12-26 19:07:27 -0300

    Get secret key from properties

--- src/main/java/com/bruno13palhano/shopdani_stock_management/JwtProperties.java
+++ src/main/java/com/bruno13palhano/shopdani_stock_management/JwtProperties.java
@@ -1,14 +1,18 @@
 package com.bruno13palhano.shopdani_stock_management;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "jwt")
+@Component
 public class JwtProperties {
 
-    private String secretKey = "rzxlszyykpbgqcflzxsqcysyhljt";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     // validity in milliseconds
-    private long validityInMs = 3600000; // 1h
+    private long validityInMs = 2592000000L; // 1h
 
     public String getSecretKey() {
         return secretKey;
